5ced41f072c184b754ac7749bdf1d3c691326e0e
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2026-01-05 10:14:36 -0600

    feat(app): add appendSync to insert new text in the file

--- 02-app/app.js
+++ 02-app/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'Hello World!');
+// fs.writeFileSync('notes.txt', 'Hello World!');
+fs.appendFileSync('notes.txt', ' Welcome to Node.js. This is a message to be appended')
